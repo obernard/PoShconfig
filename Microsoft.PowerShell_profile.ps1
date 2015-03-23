16d185dc1f7608af0c69543959a936abc2285235
Olivier BERNARD <obernard@solent.fr>
2015-03-23 10:31:36 +0100

    Added persistent history.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,17 @@
 $EDITOR_PATH = "C:\Program Files (x86)\Vim\vim74\gvim.exe"
 $HOSTS_PATH = "C:\Windows\System32\drivers\etc\hosts"
+$MAXIMUM_HISTORY_COUNT = 200
+$HISTORY_PATH = Join-Path (Split-Path $PROFILE) "history.clixml"
+
+
+Register-EngineEvent -SourceIdentifier powershell.exiting -SupportEvent -Action {
+    Get-History -Count $MAXIMUM_HISTORY_COUNT | Export-Clixml $HISTORY_PATH
+}
+
+
+if (Test-Path $HISTORY_PATH) {
+    Import-Clixml $HISTORY_PATH | Add-History
+}
 
 
 Function Start-Editor {
