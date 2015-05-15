fb8ceeb9fc3332a7ac8fd38d37f35be83e1f3415
Olivier BERNARD <obernard@solent.fr>
2015-05-15 16:43:58 +0200

    Added a tool function for checking if current path is sandboxed or not.

--- Modules/Cabal/Cabal.psm1
+++ Modules/Cabal/Cabal.psm1
@@ -13,6 +13,15 @@ Function Start-SandboxedRunHaskell {
 }
 
 
+Function Test-Sandbox {
+    if (Test-Path "cabal.sandbox.config") {
+        Write-Host -ForegroundColor DarkGreen "Sandboxed"
+    } else {
+        Write-Host -ForegroundColor DarkRed "Not sandboxed"
+    }
+}
+
+
 New-Alias -name ghcsandbox -value Start-SandboxedGHC
 New-Alias -name ghcisandbox -value Start-SandboxedGHCI
 New-Alias -name runhaskellsandbox -value Start-SandboxedRunHaskell
